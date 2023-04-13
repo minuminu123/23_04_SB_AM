300f2ec1d5bfe9225f52b9d218ff410ca285794d
minuminu123 <alsdn96321@gmail.com>
2023-04-13 15:37:37 +0900

    count 값 초기화 액션

--- src/main/java/com/KoreaIT/smw/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/smw/demo/controller/UsrHomeController.java
@@ -6,32 +6,43 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	int count = -1;
+
+	private int count;
+
+	public UsrHomeController() {
+		count = 0;
+	}
+
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요";
 	}
-	
+
 	@RequestMapping("/usr/home/main2")
 	@ResponseBody
 	public String showMain2() {
 		return "잘가";
 	}
+
 	@RequestMapping("/usr/home/main3")
 	@ResponseBody
 	public int showMain3() {
-		int sum = 1+2;
-		return sum;
+		int a = 1;
+		int b = 2;
+		return a + b;
 	}
-	
+
 	@RequestMapping("/usr/home/main4")
 	@ResponseBody
 	public int showMain4() {
-		
-		count++;
-		return count;
-		
+		return count++;
 	}
-	
+
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+		count = 0;
+		return "count의 값이 0으로 초기화 됨";
+	}
 }
