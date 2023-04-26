626200cf35a83fcad08e5ddf3123590d17c286c2
minuminu123 <alsdn96321@gmail.com>
2023-04-26 17:51:47 +0900

    상세보기 내의 조회수 증가를 ajax 를 통해 처리

--- src/main/java/com/KoreaIT/smw/demo/repository/ArticleRepository.java
+++ src/main/java/com/KoreaIT/smw/demo/repository/ArticleRepository.java
@@ -111,4 +111,14 @@ public interface ArticleRepository {
 
 	public int increaseHitCount(int id);
 
+	@Select("""
+			<script>
+				SELECT hitCount
+				FROM article
+				WHERE id = #{id}
+			</script>
+			""")
+	public int getArticleHitCount(int id);
+
 }
+
